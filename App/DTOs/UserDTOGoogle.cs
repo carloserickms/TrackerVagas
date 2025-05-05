2d995494cf3ece9e64b341d77610bc5a1990a019
carlos <carloserico71@gmail.com>
2025-05-04 23:40:53 -0300

    feat: add UserDTOGoogle class for Google account integration

---
+++ App/DTOs/UserDTOGoogle.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace App.DTOs
+{
+    public class UserDTOGoogle
+    {
+        [Required]
+        [MaxLength(40)]
+        public string userName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string password { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string rePassword { get; set; }
+
+        public string provider { get; set; }
+
+        public string providerId {get; set;}
+    }
+}
