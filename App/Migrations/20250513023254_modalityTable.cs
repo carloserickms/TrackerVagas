ac81fb02723f27e1cd8a7e30fb5478a869944113
carlos <carloserico71@gmail.com>
2025-05-13 01:03:14 -0300

    feat: create Modality table and update JobVacancy to use ModalityId

---
+++ App/Migrations/20250513023254_modalityTable.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    /// <inheritdoc />
+    public partial class modalityTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Modality",
+                table: "JobVacancy");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ModalityId",
+                table: "JobVacancy",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Modality",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modality", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobVacancy_Modality_VacancyStatusId",
+                table: "JobVacancy",
+                column: "VacancyStatusId",
+                principalTable: "Modality",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobVacancy_Modality_VacancyStatusId",
+                table: "JobVacancy");
+
+            migrationBuilder.DropTable(
+                name: "Modality");
+
+            migrationBuilder.DropColumn(
+                name: "ModalityId",
+                table: "JobVacancy");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Modality",
+                table: "JobVacancy",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
