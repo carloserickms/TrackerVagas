1cf6a91e21d6d85fc3d8625d2cb7e64f6fc14a2d
carlos <carloserico71@gmail.com>
2025-05-13 01:03:44 -0300

    fix: remove Modality string property and update JobVacancy constructor to use ModalityId

--- App/Models/JobVacancy.cs
+++ App/Models/JobVacancy.cs
@@ -5,21 +5,21 @@ namespace App.Models
         public string Title { get; set; }
         public string Link { get; set; }
         public string EnterpriseName { get; set; }
-        public string Modality {get; set;}
         public Guid VacancyStatusId {get; set;}
+        public Guid ModalityId {get; set;}
         public Guid UserId {get; set;}
 
+        public Modality Modality {get; set;}
         public VacancyStatus VacancyStatus {get; set;}
         public User User {get; set;}
 
         public JobVacancy() : base(Guid.NewGuid(), DateTime.Now, DateTime.Now) { }
 
-        public JobVacancy(string title, string link, string enterpriseName, string modality, Guid id, DateTime createdAt, DateTime updatedAt) : base(id, createdAt, updatedAt)
+        public JobVacancy(string title, string link, string enterpriseName, Guid id, DateTime createdAt, DateTime updatedAt) : base(id, createdAt, updatedAt)
         {
             Title = title;
             Link = link;
             EnterpriseName = enterpriseName;
-            Modality = modality;
         }
     }
 }
