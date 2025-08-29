fc5619aa3352cd5342fe7ae61c7e1de233bfa429
Carlos Erick Mota Dos Santos <carloserico71@gmail.com>
2025-08-29 00:59:45 -0300

    feat: add seeding methods for InterestLevel and TypeOfContract in the database

--- App/seeds/SeedData.cs
+++ App/seeds/SeedData.cs
@@ -40,3 +40,45 @@ public static class ModalityTableSeed
         }
     }
 }
+
+public static class InterestLevelSeed
+{
+    public static void Seed(AppDbContext context)
+    {
+        if (!context.InterestLevel.Any())
+        {
+            var now = DateTime.Now;
+            var interestLevel = new List<InterestLevel>
+            {
+                new InterestLevel("Muito Interessado", Guid.NewGuid(), now, now),
+                new InterestLevel("Neutro", Guid.NewGuid(), now, now),
+                new InterestLevel("Pouco Interessado", Guid.NewGuid(), now, now),
+            };
+
+            context.InterestLevel.AddRange(interestLevel);
+            context.SaveChanges();
+        }
+    }
+}
+
+public static class TypeOfContractSeed
+{
+    public static void Seed(AppDbContext context)
+    {
+        if (!context.TypeOfContract.Any())
+        {
+            var now = DateTime.Now;
+            var typeOfContract = new List<TypeOfContract>
+            {
+                new TypeOfContract("Experiência", Guid.NewGuid(), now, now),
+                new TypeOfContract("Aprendiz", Guid.NewGuid(), now, now),
+                new TypeOfContract("Estágio", Guid.NewGuid(), now, now),
+                new TypeOfContract("CLT", Guid.NewGuid(), now, now),
+                new TypeOfContract("PJ", Guid.NewGuid(), now, now),
+            };
+
+            context.TypeOfContract.AddRange(typeOfContract);
+            context.SaveChanges();
+        }
+    }
+}
