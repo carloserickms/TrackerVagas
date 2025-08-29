88ee330e49047d3fe29037ce39ce99a5f302eef7
Carlos Erick Mota Dos Santos <carloserico71@gmail.com>
2025-08-29 00:58:17 -0300

    feat: add AllInterestLevelResponseDTO and IInterestLevel interface; update JobDTO, JobResponseByIdDTO, JobResponseDTO, and UpdateJobDTO with new properties for location, salary, workload, typeOfContract, and interestLevel

--- App/DTOs/JobResponseDTO.cs
+++ App/DTOs/JobResponseDTO.cs
@@ -6,8 +6,17 @@ namespace App.DTOs
         public string title { get; set; }
         public string link { get; set; }
         public string enterpriseName { get; set; }
+        public string? location { get; set; }
+        public float? salary { get; set; }
+        public float? workload { get; set; }
+        public string? typeOfContract { get; set; }
+        public Guid? typeOfContractId { get; set; }
+        public string? interestLevel { get; set; }
+        public Guid? interestLevelId { get; set; }
         public string status { get; set; }
+        public Guid statusId { get; set; }
         public string modality { get; set; }
+        public Guid modalityId { get; set; }
         public DateTime createdAt { get; set; }
         public DateTime updatedAt { get; set; }
     }
